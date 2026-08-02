07c6dac85b32a80149222431bc6d4ed23b51994f
Qurbanov <aliqurbanov0609@icloud.com>
2026-08-02 06:54:16 +0400

    Add service worker: sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "gdigital-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      cache.addAll([
+        "/",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png",
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
